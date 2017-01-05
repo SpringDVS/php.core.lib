3e25af3365aa6ee0a9711b4267005c52ae46a120
Charlie Fyvie-Gauld <cfg@zunautica.org>
2017-01-05 16:55:44 +0000

    Add correct license header

--- src/Core/NetServices/BulletinRepositoryInterface.php
+++ src/Core/NetServices/BulletinRepositoryInterface.php
@@ -1,5 +1,8 @@
 <?php
-
+/* Notice:  Copyright 2016, The Care Connections Initiative c.i.c.
+ * Authors: Charlie Fyvie-Gauld <cfg@zunautica.org>
+ * License: Apache License, Version 2 (http://www.apache.org/licenses/LICENSE-2.0)
+ */
 namespace SpringDvs\Core\NetServices;
 
 /**
